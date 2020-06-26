7b843e810a8a1a16d8a9d1806c90e0c5d78e5d88
shvm24 <shvm24@gmail.com>
2020-06-26 21:08:05 +0530

    Advance version - infinite loop

--- corona.py
+++ corona.py
@@ -7,16 +7,35 @@ Created on Wed May 27 16:15:35 2020
 import smtplib
 import requests
 from bs4 import BeautifulSoup
-url = "https://worldometers.info/coronavirus/country/india/"
-content = requests.get(url)
-htmlContent = content.content
-soup = BeautifulSoup(htmlContent,'html.parser')
-containers = soup.find(("div"), ["maincounter-number"]).get_text()
-connection = smtplib.SMTP('smtp.gmail.com' , 587)
-connection.ehlo()
-connection.starttls()
-connection.login('reachout2shivam@gmail.com','******')
-connection.sendmail('reachout2shivam@gmail.com','shvm24@gmail.com',
-                    'India coronavirus Counter'+containers)
-connection.quit()
+class Corona:   
+    def email(self,containers):
+        self.containers=containers
+        rec_list =  ['shvm24@gmail.com','ruchisweet4@gmail.com', 'reachout2shivam@gmail.com','taresh003@gmail.com']
+        connection = smtplib.SMTP('smtp.gmail.com' , 587)
+        connection.ehlo()
+        connection.starttls()
+        connection.login('reachout2shivam@gmail.com','hanjihanji909080')
+        connection.sendmail('reachout2shivam@gmail.com',
+                            rec_list,
+                            'India coronavirus Counter has increased'+ 
+                            'Be Cautious while going out'+ str(containers))        
+        connection.quit()
+        return containers;     
+    def coronaCounter(self,oldCounter):   
+        self.oldCounter = oldCounter;            
+        url = "https://worldometers.info/coronavirus/country/india/"
+        content = requests.get(url)
+        htmlContent = content.content
+        soup = BeautifulSoup(htmlContent,'html.parser')
+        container = soup.find(("div"), ["maincounter-number"]).get_text() 
+        containers=int(container.replace(",",''))
+        if (containers > oldCounter):
+            newCount=co.email(containers) 
+            oldCounter = newCount   
+        return oldCounter
 
+
+oldCounter=0        
+co = Corona()
+oldCounter= co.coronaCounter(oldCounter)
+print(oldCounter)
